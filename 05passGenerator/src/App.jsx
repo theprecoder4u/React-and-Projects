d6f9ba975916e19221f2bb7836ca72b1f9006b09
Preetam Patel <preetampatel620@gmail.com>
2026-04-07 08:16:36 +0530

    making pass

--- 05passGenerator/src/App.jsx
+++ 05passGenerator/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const[num, setNum] = useState(false)
+  const[char, setChar] = useState(false)
+  const[password, setPass] = useState("")
 
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(num) str+="0123456789"
+    if(char) str+="!@#$%^&*~_-+="
+
+    for (let i = 0; i < str.length; i++) {
+      let char = Math.floor(Math.random() *str.length + 1)
+      pass = str.charAt(char)
+    }
+
+    setPass(pass)
+
+  }, [length,num,char,setPass])
   return (
     <>
-    <h1 className='text-4xl text-center
-     text-white'>Password Generator</h1>
+    <div className='w-full max-w-md mx-auto shadow-md
+    rounded-lg px-4 my-8 text-orange-500 bg-gray'>
+      test
+    </div>
     </>
   )
 }
-
 export default App
